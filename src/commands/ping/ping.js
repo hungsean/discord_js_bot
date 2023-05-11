6bfc23178ad724a5e9ebfdf8e2afdb96e4fee02a
hungsean <hungsean0419@gmail.com>
2023-05-11 21:38:08 +0800

    add some interesting reply

--- src/commands/ping/ping.js
+++ src/commands/ping/ping.js
@@ -1,11 +1,20 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+const wait = require('node:timers/promises').setTimeout;
+
+
 module.exports = {
 	cooldown: 5,
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		await interaction.deferReply({ ephemeral: true });
+		await interaction.editReply('Pong!');
+		await wait(2000);
+		await interaction.followUp({ content: 'Pong again!', ephemeral: true });
+		const message = await interaction.fetchReply();
+		await interaction.deleteReply();
+		console.log(message);
 	},
 };
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -6,6 +6,7 @@ module.exports = {
 
 		if (!interaction.isChatInputCommand()) return;
 
+
 		const command = interaction.client.commands.get(interaction.commandName);
 
 		if (!command) {
