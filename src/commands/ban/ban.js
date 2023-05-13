aa2b7f79b6b9efda0d6ec166bac49835b7db539c
hungsean <hungsean0419@gmail.com>
2023-05-14 01:11:30 +0800

    add unban

--- src/commands/ban/ban.js
+++ src/commands/ban/ban.js
@@ -3,23 +3,53 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ban')
-		.setDescription('Select a member and ban them.')
-		.addUserOption(option =>
-			option
-				.setName('target')
-				.setDescription('The member to ban')
-				.setRequired(true))
-		.addStringOption(option =>
-			option
-				.setName('reason')
-				.setDescription('The reason for banning'))
+		.setDescription('Select a member and ban them or unban.')
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('unban')
+				.setDescription('Select a member and unban them.')
+				.addUserOption(option =>
+					option
+						.setName('target')
+						.setDescription('The member to ban or unban')
+						.setRequired(true)
+				)
+				.addStringOption(option =>
+					option
+						.setName('reason')
+						.setDescription('The reason for banning')
+				)
+			)
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('ban')
+				.setDescription('Select a member and unban them.')
+				.addUserOption(option =>
+					option
+						.setName('target')
+						.setDescription('The member to ban or unban')
+						.setRequired(true)
+				)
+				.addStringOption(option =>
+					option
+						.setName('reason')
+						.setDescription('The reason for banning')
+				)
+		)
 		.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
 		.setDMPermission(false),
     async execute(interaction) {
         const target = interaction.options.getUser('target');
         const reason = interaction.options.getString('reason') ?? 'No reason provided';
-
-        await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
-        await interaction.guild.members.ban(target);
+		if (interaction.options.getSubcommand() === 'ban')
+		{
+			await interaction.reply(`Banning ${target.username} for reason: ${reason}`);
+			await interaction.guild.members.ban(target);
+		}
+		else if (interaction.options.getSubcommand() === 'unban')
+		{
+			await interaction.reply(`Unbanning ${target.username} for reason: ${reason}`);
+			await interaction.guild.members.unban(target);
+		}
     },
 };
