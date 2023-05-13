0b8f409cc1bd558ce5fe169fa03dacf96a6ec10b
hungsean <hungsean0419@gmail.com>
2023-05-13 16:38:32 +0800

    add echo, info, and ban

---
+++ src/commands/echo/echo.js
@@ -0,0 +1,53 @@
+const { SlashCommandBuilder, ChannelType } = require('discord.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('echo')
+		.setDescription('Replies with your input!')
+		.addStringOption(option =>
+			option
+				.setName('input')
+				.setDescription('The input to echo back')
+				.setRequired(true)
+			)
+		.addStringOption(option =>
+				option
+					.setName('type')
+					.setRequired(true)
+					.addChoices(
+						{ name: 'once', value: 'once' },
+						{ name: 'twice', value: 'twice' },
+					)
+		)
+		.addChannelOption(option =>
+                option
+                    .setName('channel')
+                    .setDescription('The channel to echo into')
+					.addChannelTypes(ChannelType.GuildText)
+            )
+        .addBooleanOption(option =>
+                option
+                    .setName('ephemeral')
+                    .setDescription('Whetheo or nor the echo should be ephemeral')
+            ),
+	async execute(interaction) {
+		const input = interaction.options.getString('input', true);
+		const type = interaction.options.getString('type', true);
+		const ephemeral = interaction.options.getBoolean('ephemeral', false);
+		// const channel = interaction.options.getChannel('channel', false);
+		if (type == 'once')
+		{
+			await interaction.reply({ content: input, ephemeral: ephemeral });
+		}
+		else if (type == 'twice')
+		{
+			await interaction.reply({ content: input, ephemeral: ephemeral });
+			await interaction.reply({ content: input, ephemeral: ephemeral });
+		}
+		else
+		{
+			await interaction.reply({ content: 'error type', ephemeral: ephemeral });
+		}
+	},
+};
