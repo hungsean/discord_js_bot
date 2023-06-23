86707ef86927acd65c1821b756737b499dff7e87
hungsean <hungsean0419@gmail.com>
2023-06-23 14:26:30 +0800

    做成可以多回應的指令

--- src/commands/server/index.js
+++ src/commands/server/index.js
@@ -6,7 +6,50 @@ module.exports = {
 		.setName('server')
 		.setDescription('Provides information about the server.'),
 	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		await interaction.reply('Please enter more input.');
+		console.log("new one ---")
+		const collectorFilter = m => {
+			return interaction.user.id === m.author.id;
+			// console.log(interaction.user.id);
+			// console.log(m.author.id);
+			// console.log(interaction.user.id === m.author.id);
+		};
+		try 
+		{
+			const messages = await interaction.channel.awaitMessages({ 
+				filter: collectorFilter, 
+				time: 10000, 
+				max: 1, 
+				errors: ['time'] 
+			});
+			console.log(messages.first());
+			interaction.followUp(`You've entered: ${messages.first().content}`);
+
+		} 
+		catch (error) 
+		{
+			interaction.followUp('You did not enter any input!');
+		}
+
+
+
+		// await interaction.reply('Please enter more input.');
+
+		// interaction.reply('Please enter more input.').then(() => {
+		// 	const collectorFilter = m => {
+		// 		interaction.user.id === m.author.id
+		// 		console.log(interaction.user.id);
+		// 		console.log(m.author.id);
+		// 		console.log(interaction.user.id === m.author.id);
+		// 	};
+		
+		// 	interaction.channel.awaitMessages({ filter: collectorFilter, time: 10000, max: 1, errors: ['time'] })
+		// 		.then(messages => {
+		// 			interaction.followUp(`You've entered: ${messages.first().content}`);
+		// 		})
+		// 		.catch(() => {
+		// 			interaction.followUp('You did not enter any input!');
+		// 		});
+		// });
 	},
 };
