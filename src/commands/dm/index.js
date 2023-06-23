6b587ce02467efa73ec40adcbd5fd898241d4412
hungsean <hungsean0419@gmail.com>
2023-06-23 14:26:02 +0800

    外加了可以傳私訊的command

--- src/commands/dm/index.js
+++ src/commands/dm/index.js
@@ -2,9 +2,10 @@ const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('echo')
-		.setDescription('Replies with your input!'),
+		.setName('dm')
+		.setDescription('dm you'),
 	async execute(interaction) {
-		
+		interaction.user.send('hello');
+        interaction.reply({ ephemeral: true, content: 'dm you' });
 	},
 };
