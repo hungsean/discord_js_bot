a6c7e7acb7e8908af61b8a22035e1bcbf2d92b38
hungsean <hungsean0419@gmail.com>
2023-07-01 16:21:29 +0800

    let ping more possible

--- src/commands/ping/index.js
+++ src/commands/ping/index.js
@@ -9,13 +9,16 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.deferReply({ ephemeral: true });
-		await interaction.editReply('Pong!');
-		await wait(2000);
-		await interaction.followUp({ content: 'Pong again!', ephemeral: true });
-		const message = await interaction.fetchReply();
-		await interaction.deleteReply();
-		console.log(message);
-		await interaction.channel.send('ping: ' + interaction.client.ws.ping + 'ms');
+		const sent = await interaction.reply({ content: 'Pinging...', fetchReply: true });
+		// await interaction.deferReply({ ephemeral: true });
+		// await interaction.editReply('Pong!');
+		// await wait(2000);
+		// await interaction.followUp({ content: 'Pong again!', ephemeral: true });
+		// const message = await interaction.fetchReply();
+		// await interaction.deleteReply();
+		// console.log(message);
+		// await interaction.channel.send('ping: ' + interaction.client.ws.ping + 'ms');
+		
+		interaction.editReply(`Roundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);	
 	},
 };
